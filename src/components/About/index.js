7200ae86cc5a4f9425600d02bd628878eba29c28
pra18apr <pra.18apr@gmail.com>
2023-09-23 16:28:00 -0600

    added about file to project

---
+++ src/components/About/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Pradeep Kumar</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/130611291?v=4" alt="Pradeep Kumar" />
+
+                <p>
+                An aspiring Full stack web developer pursuing Full stack web developer Bootcamp from University of Toronto. <br />
+                Seasoned Accountant with over 7 years of experience. 
+                </p>
+                <p>
+                Strong knowledge of frontend and backend web development technologies, such as <br />
+                HTML, CSS, JS, Node.js, OOP, SQL, ORM, MVC, NOSQL, PWA, Reach, MERN & State.
+                </p>
+                <p>
+                Proven leadership qualities to lead a IT / App / Web develeopment Project Manager role.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
